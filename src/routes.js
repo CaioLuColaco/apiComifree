1215175a1faba10158d564efda9a3104c064e90f
caio lucena <caio.lcolaco@gmail.com>
2022-12-11 20:20:16 -0300

    registred the routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,27 @@
 const express = require("express")
 
-const router = express.Router()
+const routes = express.Router()
 
+const ProductControllers = require("./controllers/ProductControllers")
+const CategoryControllers = require("./controllers/CategoryControllers")
+const CompanyControllers = require("./controllers/CompanyControllers")
+
+// Products
+routes.get("/products",             ProductControllers.findAll)
+routes.get("/product/:id",          ProductControllers.find)
+routes.post("/product",             ProductControllers.update)
+routes.delete("/product/:id",       ProductControllers.delete)
+
+// Category
+routes.get("/categories",           CategoryControllers.findAll)
+routes.get("/category/:id",         CategoryControllers.find)
+routes.post("/category",            CategoryControllers.update)
+routes.delete("/category/:id",      CategoryControllers.delete)
+
+// Company
+routes.get("/companies",            CompanyControllers.findAll)
+routes.get("/company/:id",          CompanyControllers.find)
+routes.post("/company",             CompanyControllers.update)
+routes.delete("/company/:id",       CompanyControllers.delete)
+
+module.exports = routes
