4ce17146c8d15fe10d1717afd9542d5b41558ed1
caio lucena <caio.lcolaco@gmail.com>
2022-12-11 20:21:08 -0300

    added the routes on server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const routes = require('./src/routes');
 
 const app = express()
 
 app.use(cors());
 app.use(express.json())
+app.use(routes)
 
 app.listen(process.env.PORT || 3001, function(){
     console.log(`SERVER IS RUNNIN ON PORT: ${this.adress().port}`)
